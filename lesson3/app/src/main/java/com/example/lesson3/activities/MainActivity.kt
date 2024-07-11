ba5b8645cba3c772e7cd4f5aa4e838b0ba92b3c0
Tiris <Vil-geforts@yandex.ru>
2024-07-11 19:53:27 +0400

    home task №3

---
+++ lesson3/app/src/main/java/com/example/lesson3/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lesson3.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lesson3.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
