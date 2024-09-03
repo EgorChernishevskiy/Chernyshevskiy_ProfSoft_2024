e7df73b84ac25167cfe3a71a23b2e81fd41b37e0
Tiris <100187552+EgorChernishevskiy@users.noreply.github.com>
2024-09-03 10:50:16 +0400

    Error from last commit has been fixed

--- TogetherApp/domain/src/main/java/com/example/togetherapp/domain/usecase/profile/GetAllUserProfilesUseCase.kt
+++ TogetherApp/domain/src/main/java/com/example/togetherapp/domain/usecase/profile/GetAllUserProfilesUseCase.kt
@@ -1,10 +1,11 @@
 package com.example.togetherapp.domain.usecase.profile
 
+import com.example.togetherapp.domain.model.profile.ProfilePreview
 import com.example.togetherapp.domain.model.profile.UserProfile
 import com.example.togetherapp.domain.repository.UserProfileRepository
 
 class GetAllUserProfilesUseCase(private val repository: UserProfileRepository) {
-    suspend fun execute(): List<UserProfile> {
+    suspend fun execute(): List<ProfilePreview> {
         return repository.getAllUserProfiles()
     }
 }
